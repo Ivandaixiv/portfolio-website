f4d12203511df252182bb4a1953eef2ea907065a
Ivan <ivandaixivwork@gmail.com>
2020-03-11 16:33:55 -0700

    Updates

---
+++ precache-manifest.91338de72d5788010e5b7bce67ccb014.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d675cd6cb24007e4ede21de6f0d72cf1",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "e9c1ce4aac751de8c1a5",
+    "url": "/portfolio-website/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "185610170ef6d872fda7",
+    "url": "/portfolio-website/static/js/2.f77f5db6.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/portfolio-website/static/js/2.f77f5db6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9c1ce4aac751de8c1a5",
+    "url": "/portfolio-website/static/js/main.72d003e7.chunk.js"
+  },
+  {
+    "revision": "2517a8b4628c70fa0925",
+    "url": "/portfolio-website/static/js/runtime-main.6a698c4e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
