44b9e54391043e848df441014f43280187568ba8
Ivan <ivandaixivwork@gmail.com>
2020-05-14 11:08:03 -0700

    Adds content to the desktop site

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -20,30 +20,31 @@ const Home = (props) => {
     <div className={classes.container}>
       <img src={profile} alt="Ivan Dai" className={classes.profile} />
       <Introduction />
+      <h1>Heres what people had to say</h1>
       <Slider dots>
-        <div>
-          <p>
+        <div className={classes.slide}>
+          <p className={classes.slideText}>
             The amount of talent Ivan has astounded me. Ivan and I worked on
             many projects together at Red Academy. His ability to solve and
             navigate coding problems is impressive to see. As a team member,
             Ivan has my highest recommendation. It was a pleasure working with
             you Ivan.
           </p>
-          <h2>Lance Patterson</h2>
+          <h2 className={classes.slideName}>Lance Patterson</h2>
           <img
             src={Lance}
             alt="Lance Patterson"
             className={classes.testimonialImage}
           />
         </div>
-        <div>
-          <p>
+        <div className={classes.slide}>
+          <p className={classes.slideText}>
             I had a great time working with Ivan during our Meteor-React
             project. He's extremely efficient with his work process and was
             crushing the assignments in our time together. He is someone that
             you can depend on and I definitely recommend him.
           </p>
-          <h2>Ringo Wong</h2>
+          <h2 className={classes.slideName}>Ringo Wong</h2>
           <img
             src={Ringo}
             alt="Ringo Wong"
@@ -54,8 +55,40 @@ const Home = (props) => {
     </div>
   ) : (
     <div className={classes.container}>
-      <img src={profile} alt="Ivan Dai" />
+      <img src={profile} alt="Ivan Dai" className={classes.profile} />
       <Introduction />
+      <h1>Heres what people had to say</h1>
+      <Slider dots>
+        <div className={classes.slide}>
+          <p className={classes.slideText}>
+            The amount of talent Ivan has astounded me. Ivan and I worked on
+            many projects together at Red Academy. His ability to solve and
+            navigate coding problems is impressive to see. As a team member,
+            Ivan has my highest recommendation. It was a pleasure working with
+            you Ivan.
+          </p>
+          <h2 className={classes.slideName}>Lance Patterson</h2>
+          <img
+            src={Lance}
+            alt="Lance Patterson"
+            className={classes.testimonialImage}
+          />
+        </div>
+        <div className={classes.slide}>
+          <p className={classes.slideText}>
+            I had a great time working with Ivan during our Meteor-React
+            project. He's extremely efficient with his work process and was
+            crushing the assignments in our time together. He is someone that
+            you can depend on and I definitely recommend him.
+          </p>
+          <h2 className={classes.slideName}>Ringo Wong</h2>
+          <img
+            src={Ringo}
+            alt="Ringo Wong"
+            className={classes.testimonialImage}
+          />
+        </div>
+      </Slider>
     </div>
   );
 };
