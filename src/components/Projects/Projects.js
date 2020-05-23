5f188a4c93ad9079a07a4f8e5f6e9329f47e3bd8
Ivan <ivandaixivwork@gmail.com>
2020-05-23 12:33:58 -0700

    Add ProjectList as a component

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,10 +1,19 @@
 import React from "react";
-import Github from "react-ionicons/lib/LogoGithub";
 import { withStyles } from "@material-ui/core/styles";
 import styles from "./styles";
 import Fade from "react-reveal/Fade";
+import ProjectsList from "../ProjectsList";
 const Projects = ({ classes, data, events }) => {
-  return <p>Projects</p>;
+  return (
+    <Fade>
+      <div>
+        <h2>Featured</h2>
+      </div>
+      <div>
+        <ProjectsList />
+      </div>
+    </Fade>
+  );
 };
 
 export default withStyles(styles)(Projects);
---
+++ src/components/ProjectsList/ProjectsList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import styles from "./styles";
+const ProjectsList = ({ classes, data, events }) => {
+  return (
+    <ul>
+      <li>Yes</li>
+    </ul>
+  );
+};
+
+export default withStyles(styles)(ProjectsList);
