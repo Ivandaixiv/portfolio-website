1008f5aaee3c79afebf29c95122f7f0d49f84a43
Ivan <ivandaixivwork@gmail.com>
2020-05-24 12:59:11 -0700

    Add photo of projects to local

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { withStyles } from "@material-ui/core/styles";
 import styles from "./styles";
 import Fade from "react-reveal/Fade";
 import ProjectsList from "../ProjectsList";
-const Projects = ({ classes, data, events }) => {
+import Voicestory from "../../assets/voicestory.gif";
+const Projects = ({ classes }) => {
+  const data = [
+    {
+      name: "Voicestory 2020",
+      video: Voicestory,
+    },
+  ];
+  const [selectedProject, setSelectedProject] = useState(data[0]);
+
   return (
     <Fade>
       <div>
         <h2>Featured</h2>
+        <p>{selectedProject.name}</p>
+        <img
+          src={selectedProject.video}
+          alt={selectedProject.name}
+          style={{ borderRadius: 40 }}
+        />
       </div>
       <div>
-        <ProjectsList />
+        <ProjectsList setSelectedProject={setSelectedProject} data={data} />
       </div>
     </Fade>
   );
--- src/components/ProjectsList/ProjectsList.js
+++ src/components/ProjectsList/ProjectsList.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 import styles from "./styles";
-const ProjectsList = ({ classes, data, events }) => {
+const ProjectsList = ({ classes, setSelectedProject }) => {
   return (
     <ul>
       <li>Yes</li>
+      <button
+        onClick={() => {
+          setSelectedProject({
+            name: "NEWWW 2020",
+            video: "dsassd",
+          });
+        }}
+      />
     </ul>
   );
 };
